b308c531fabd388bf5bcf2c85f60c4a7f2febb4c
Ronnie Lee <rlmorgan512gmail.com>
2026-08-21 11:19:37 -0500

    Fix root import path

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import sys
+import os
+
+# Points Python to the root directory where main.py sits
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
