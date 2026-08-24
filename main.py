93233e9681a7d24dc71d24fe32bf2e8c570ea3df
Ronnie Lee <rlmorgan512gmail.com>
2026-08-23 20:25:06 -0500

    Direct routing setup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask
 
-# Tell Flask exactly where the templates folder lives in Vercel
 base_dir = os.path.abspath(os.path.dirname(__file__))
 template_dir = os.path.join(base_dir, 'templates')
 
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__, template_folder=template_dir)if __name__ == '__main__':
+    app.run()
